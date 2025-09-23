4130415bfc03053e92fa7167de551e0d410412f6
Abril Palau Pardillos <abrilpalau14@gmail.com>
2025-09-23 16:48:05 +0200

    first part of the exercise 1

---
+++ ejercicio1.py
@@ -0,0 +1,22 @@
+trabajadores = input("trabajadores?")
+num_trabajadores = int(trabajadores)
+print(trabajadores);
+
+hora_salida = input("hora de salida?")
+print(hora_salida);
+
+x = 5
+
+while num_trabajadores > 0:
+    print (num_trabajadores)
+    num_trabajadores = num_trabajadores -1
+    nombre_empleado = input("nombre empleado?")
+    hora_entrada = input("hora entrada?")
+    hora_salida = input("hora salida?")
+
+    if hora_entrada > hora_salida:
+        print("hora correcta")
+    else:
+        print("hora incorrecta")
+else:
+    print("final!");
